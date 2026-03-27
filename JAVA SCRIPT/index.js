bec4280e01bf0a27f63316c6f7f92034981a1464
Theerthananda <theerthanandagowda@gmail.com>
2026-03-27 13:00:39 +0530

    first commit:

---
+++ JAVA SCRIPT/index.js
@@ -0,0 +1,7 @@
+//its my first js file and today means friday march 27,2026 i started core javascript and i am very excited to learn it and i will try my best to learn it and i will also try to make some projects in javascript and i will also try to learn some frameworks in javascript like react and angular and vue and node js and express js and mongodb and mysql and postgresql and redis and docker and kubernetes and aws and azure and google cloud platform and many more technologies that are related to javascript and web development.
+
+//i will also try to learn some programming languages like python and java and c++ and c# and ruby and php and go and rust and many more programming languages that are related to web development and software development.
+
+//i will also try to learn some tools like git and github and vscode and intellij and pycharm and webstorm and many more tools that are related to web development and software development.    
+
+console.log("Hello World!");
