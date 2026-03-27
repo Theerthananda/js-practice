bb946d9f8833455f18017594f47617e9e197ee30
Theerthananda <theerthanandagowda@gmail.com>
2026-03-27 20:30:51 +0530

    java script

--- JAVA SCRIPT/index.js
+++ JAVA SCRIPT/index.js
@@ -5,3 +5,18 @@
 //i will also try to learn some tools like git and github and vscode and intellij and pycharm and webstorm and many more tools that are related to web development and software development.    
 
 console.log("Hello World!");
+
+let name = "John"; //let is used to declare a variable that can be reassigned later. and the main difference between let and var is that let is block scoped and var is function scoped. and the main difference between let and const is that let can be reassigned later and const cannot be reassigned later.
+// also recommend to use const instead of let if you don't want to reassign the variable later. and also recommend to use let instead of var because var is function scoped and can cause some issues in the code.
+
+//the main purpose of using let and const is to declare variables in javascript and to store values in those variables and to use those variables later in the code. and also to make the code more readable and maintainable. and also to avoid some issues that can be caused by using var. 
+//how let and  const deesides what type of variable it is and how it can be used in the code. and also to make the code more efficient and optimized. and also to make the code more secure and less prone to errors. and also to make the code more modular and reusable. and also to make the code more scalable and maintainable. and also to make the code more readable and understandable. and also to make the code more consistent and standardized. and also to make the code more professional and industry standard. and also to make the code more efficient and optimized. and also to make the code more secure and less prone to errors. and also to make the code more modular and reusable. and also to make the code more scalable and maintainable. and also to make the code more readable and understandable. and also to make the code more consistent and standardized. and also to make the code more professional and industry standard.
+
+var age = 30; //var is used to declare a variable that can be reassigned later. and the main difference between var and let is that var is function scoped and let is block scoped. and the main difference between var and const is that var can be reassigned later and const cannot be reassigned later.
+
+const pi = 3.14; //const is used to declare a variable that cannot be reassigned later. and the main difference between const and let is that const cannot be reassigned later and let can be reassigned later. and the main difference between const and var is that const cannot be reassigned later and var can be reassigned later.
+
+console.log(name); //John
+console.log(age); //30
+console.log(pi); //3.14 
+console.log("This is a test"); //this is a test
