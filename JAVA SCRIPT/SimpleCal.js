97208a6a00c8a6e5cf9d31cb899b04eb2fbb3d7f
Theerthananda <theerthanandagowda@gmail.com>
2026-03-30 22:50:44 +0530

    heyyy!!! added simple calculator app

---
+++ JAVA SCRIPT/SimpleCal.js
@@ -0,0 +1,35 @@
+
+
+function add(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 + num2;
+    document.getElementById("result").innerHTML = "Result: " + result;  
+}
+
+function subtract(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 - num2;
+    document.getElementById("result").innerHTML = "Result: " + result;  
+}
+
+function multiply(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 * num2;
+    document.getElementById("result").innerHTML = "Result: " + result;  
+}
+
+function divide(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    if(num2 === 0){
+        document.getElementById("result").innerHTML = "<font color='red'>Error: Division by zero is not allowed.</font>";
+        return;
+    }
+    let result = num1 / num2;
+    document.getElementById("result").innerHTML = "Result: " + result;  
+}
+
